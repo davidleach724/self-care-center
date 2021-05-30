9697fcd009e9a8ff9765df0ad0f61e31cbd225f4
David Leach <davidleach724@gmail.com>
2021-05-30 09:14:56 -0600

    Clean up form redundancy

--- main.js
+++ main.js
@@ -17,42 +17,41 @@ var formCustomMessageCenter = document.getElementById('custom-message-center');
 
 //data
 var affirmations = [
-  "I forgive myself and set myself free.",
-  "I believe I can be all that I want to be.",
-  "I am in the process of becoming the best version of myself.",
-  "I have the freedom & power to create the life I desire.",
-  "I choose to be kind to myself and love myself unconditionally.",
-  "My possibilities are endless.",
-  "I am worthy of my dreams.",
-  "I am enough.",
-  "I deserve to be healthy and feel good.",
-  "I am full of energy and vitality and my mind is calm and peaceful.",
-  "Every day I am getting healthier and stronger.",
-  "I honor my body by trusting the signals that it sends me.",
-  "I manifest perfect health by making smart choices.",
+  'I forgive myself and set myself free.',
+  'I believe I can be all that I want to be.',
+  'I am in the process of becoming the best version of myself.',
+  'I have the freedom & power to create the life I desire.',
+  'I choose to be kind to myself and love myself unconditionally.',
+  'My possibilities are endless.',
+  'I am worthy of my dreams.',
+  'I am enough.',
+  'I deserve to be healthy and feel good.',
+  'I am full of energy and vitality and my mind is calm and peaceful.',
+  'Every day I am getting healthier and stronger.',
+  'I honor my body by trusting the signals that it sends me.',
+  'I manifest perfect health by making smart choices.',
 ];
 
 var mantras = [
-  "Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.",
-  "Don’t let yesterday take up too much of today.",
-  "Every day is a second chance.",
-  "Tell the truth and love everyone.",
-  "I am free from sadness.",
-  "I am enough.",
-  "In the beginning it is you, in the middle it is you and in the end it is you.",
-  "I love myself.",
-  "I am present now.",
-  "Inhale the future, exhale the past.",
-  "This too shall pass.",
-  "Yesterday is not today.",
-  "The only constant is change.",
-  "Onward and upward.",
-  "I am the sky, the rest is weather.",
+  'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
+  'Don’t let yesterday take up too much of today.',
+  'Every day is a second chance.',
+  'Tell the truth and love everyone.',
+  'I am free from sadness.',
+  'I am enough.',
+  'In the beginning it is you, in the middle it is you and in the end it is you.',
+  'I love myself.',
+  'I am present now.',
+  'Inhale the future, exhale the past.',
+  'This too shall pass.',
+  'Yesterday is not today.',
+  'The only constant is change.',
+  'Onward and upward.',
+  'I am the sky, the rest is weather.',
 ];
 
-
 //event listeners
-buttonReceiveMessage.addEventListener("click", displayMessage);
+buttonReceiveMessage.addEventListener('click', displayMessage);
 buttonAddMessage.addEventListener('click', showCustomForm);
 buttonSubmitMessage.addEventListener('click', compileNewMessage);
 
@@ -63,54 +62,45 @@ function getRandomIndex(array) {
 
 function displayMessage() {
   if (radioMantra.checked || radioAffirmation.checked) {
-    if (!meditationBell.classList.contains("hidden")) {
-      meditationBell.classList.add("hidden");
-      displayedMessage.classList.remove("hidden");
-    }
-    if (radioAffirmation.checked) {
-      displayedMessage.innerText = affirmations[getRandomIndex(affirmations)];
-    }
-    if (radioMantra.checked) {
-      displayedMessage.innerText = mantras[getRandomIndex(mantras)];
-    }
+    meditationBell.classList.add('hidden');
+    displayedMessage.classList.remove('hidden');
+  }
+  if (radioAffirmation.checked) {
+    displayedMessage.innerText = affirmations[getRandomIndex(affirmations)];
+  }
+  if (radioMantra.checked) {
+    displayedMessage.innerText = mantras[getRandomIndex(mantras)];
   }
 }
 
-
 function showCustomForm() {
-  if (!meditationBell.classList.contains("hidden")) {
-    meditationBell.classList.add("hidden");
-  }
-  if(!displayedMessage.classList.contains("hidden")) {
-    displayedMessage.classList.add("hidden");
-  }
+  meditationBell.classList.add('hidden');
+  displayedMessage.classList.add('hidden');
   formRadioButtons.classList.add('hidden');
   buttonAddMessage.classList.add('hidden');
   formCustomMessageCenter.classList.remove('hidden');
 }
 
-
 function compileNewMessage() {
   var customMessage = inputMessage.value;
 
-  if(inputTypeOfMessage.value === 'Affirmation') {
-    radioAffirmation.checked = true;
+  if (customMessage === 'Affirmation') {
     affirmations.push(customMessage);
-  } else if(inputTypeOfMessage.value === 'Mantra') {
-      radioMantra.checked = true;
-      mantras.push(customMessage);
-    } else {
-      alert("select a message type");
-      return
-    }
+    radioAffirmation.checked = true;
+  } else if (customMessage === 'Mantra') {
+    mantras.push(customMessage);
+    radioMantra.checked = true;
+  } else {
+    return alert('Please select a message type');
+  }
   displayCustomMessage(customMessage);
-  }
+}
 
-  function displayCustomMessage(message) {
-    formRadioButtons.classList.remove('hidden');
-    buttonAddMessage.classList.remove('hidden');
-    displayedMessage.classList.remove('hidden');
-    formCustomMessageCenter.classList.add('hidden');
+function displayCustomMessage(message) {
+  formRadioButtons.classList.remove('hidden');
+  buttonAddMessage.classList.remove('hidden');
+  displayedMessage.classList.remove('hidden');
+  formCustomMessageCenter.classList.add('hidden');
 
-    displayedMessage.innerText = message;
-  }
+  displayedMessage.innerText = message;
+}
